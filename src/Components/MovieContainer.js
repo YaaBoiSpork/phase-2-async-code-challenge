368df268fbd1866a1501805051bd1f28100a7f57
Stephen <stephen.a.nerby@gmail.com>
2022-08-18 04:11:08 -0400

    Render the list of movies

--- src/Components/MovieContainer.js
+++ src/Components/MovieContainer.js
@@ -3,13 +3,16 @@ import MovieCard from "./MovieCard";
 import Filter from "./Filter";
 import Search from "./Search";
 
-function MovieContainer() {
+function MovieContainer({ movies }) {
+
+  const movieLib = movies.map(movie => <MovieCard key={movie.id} {...movie}/>)
+
   return (
     <div>
       <h1>Watchlist</h1>
       <Search /><br/>
       <Filter />
-      {/* render a list of <MovieCard> components in here */} 
+      {movieLib} 
     </div>
   );
 }
